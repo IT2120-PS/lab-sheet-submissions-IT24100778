312b9226c4d08de3bb82515a440bc7171fb53995
IT24100778 <it24100778@my.sliit.lk>
2025-09-16 21:15:16 +0530

    IT24100778_Lab07

---
+++ Lab Sheet 07/IT24100778.R
@@ -0,0 +1,21 @@
+setwd("C:\\Users\\ASUS\\OneDrive\\Desktop\\IT24100778")
+
+# EX 02
+
+## Question 01
+punif(10, min = 0, max = 40, lower.tail = FALSE) - punif(25, min = 0, max = 40, lower.tail = FALSE)
+
+## Question 02
+
+pexp(2, rate = 1/3,lower.tail = TRUE)
+
+## Question 03
+
+### Part 01
+
+1-pnorm(130,mean = 100, sd = 15, lower.tail = TRUE)
+
+### Part 02
+
+qnorm(0.95, mean=100, sd=15)
+
